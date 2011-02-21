25aa9ae23832ce0b812fddb7d930b7e15e9dca31
Christian Stocker <me@chregu.tv>
2011-02-21 16:32:34 +0100

    first commit

---
+++ example.php
@@ -0,0 +1,28 @@
+<?php
+include_once("GoogleAuthenticator.php");
+
+$secret = 'XVQ2UIGO75XRUKJO';
+$time = floor(time() / 30);
+$code = "846474";
+
+$g = new GoogleAuthenticator();
+
+print "Current Code is: ";
+print $g->getCode($secret);
+
+print "\n";
+
+print "Check if $code is valid: ";
+
+if ($g->checkCode($secret,$code)) {
+    print "YES \n";   
+} else {
+    print "NO \n";
+}
+
+$secret = $g->generateSecret();
+print "Get a new Secret: $secret \n";
+
+print "The QR Code for this secret (to scan with the Google Authenticator App: \n";
+print $g->getURL('chregu','example.org',$secret);
+print "\n";
