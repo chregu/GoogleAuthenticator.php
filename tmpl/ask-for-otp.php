07108c400a612c8ad7017837aba3e70841adafe2
Christian Stocker <me@chregu.tv>
2011-08-29 10:37:50 +0200

    add a message about the OTP in the login

--- tmpl/ask-for-otp.php
+++ tmpl/ask-for-otp.php
@@ -2,6 +2,11 @@
 <h1>please otp</h1>
 <p>
 <form method="post" action="./">
+<?php if ($debug) { ?>
+    <br/>
+    (Set $debug in index.php to false, if you don't want to have the OTP prefilled (for real life application, for example ;))<br/>
+<?php } ?>
+
 otp: <input name="otp"
 value="<?php 
 if ($debug) {
@@ -11,4 +16,5 @@ echo $g->getCode($user->getSecret());
 ?>"/><br/>
 <input type="checkbox" name="remember" id="remember" /><label for="remember"> Remember verification for this computer for 1 day.</label> <br/>
 <input type="submit"/>
+
 </form>
