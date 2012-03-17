158f8568d2282c5cc2548f7bf39e70eb64c8d26e
Thomas Rabaix <thomas.rabaix@ekino.com>
2012-03-17 22:47:33 +0100

    refactor code to PHP 5.3

--- sample/example.php
+++ sample/example.php
@@ -1,11 +1,13 @@
 <?php
-include_once("lib/GoogleAuthenticator.php");
+
+include_once(__DIR__."/../lib/Google/Authenticator/FixedBitNotation.php");
+include_once(__DIR__."/../lib/Google/Authenticator/GoogleAuthenticator.php");
 
 $secret = 'XVQ2UIGO75XRUKJO';
 $time = floor(time() / 30);
 $code = "846474";
 
-$g = new GoogleAuthenticator();
+$g = new \Google\Authenticator\GoogleAuthenticator();
 
 print "Current Code is: ";
 print $g->getCode($secret);
@@ -14,15 +16,15 @@ print "\n";
 
 print "Check if $code is valid: ";
 
-if ($g->checkCode($secret,$code)) {
-    print "YES \n";   
+if ($g->checkCode($secret, $code)) {
+    print "YES \n";
 } else {
     print "NO \n";
 }
 
 $secret = $g->generateSecret();
 print "Get a new Secret: $secret \n";
-
 print "The QR Code for this secret (to scan with the Google Authenticator App: \n";
-print $g->getURL('chregu','example.org',$secret);
+
+print $g->getURL('chregu', 'example.org', $secret);
 print "\n";
--- sample/web/Users.php
+++ sample/web/Users.php
@@ -1,139 +1,144 @@
 <?php
 
-class Users {
-    
-    
-    function __construct($file = "../users.dat") {
+class Users
+{
+    public function __construct($file = "../users.dat")
+    {
         $this->userFile = $file;
-        
-        $this->users = json_decode(file_get_contents($file),true);   
+
+        $this->users = json_decode(file_get_contents($file), true);
     }
-    function hasSession() {
+
+    public function hasSession()
+    {
         session_start();
         if (isset($_SESSION['username'])) {
             return $_SESSION['username'];
         }
         return false;
     }
-    
-    
-    function storeData(User $user) {
+
+    public function storeData(User $user)
+    {
         $this->users[$user->getUsername()] = $user->getData();
-        file_put_contents($this->userFile,json_encode($this->users));
+        file_put_contents($this->userFile, json_encode($this->users));
     }
-    
-    function loadUser($name) {
+
+    public function loadUser($name)
+    {
         if (isset($this->users[$name])) {
-            
-            return new User($name,$this->users[$name]);
-        } else {
-            return false;
+            return new User($name, $this->users[$name]);
         }
+
+        return false;
     }
-    
-    
-    
 }
 
-class User {
-    
-    function __construct($user,$data) {
+class User
+{
+    public function __construct($user, $data)
+    {
         $this->data = $data;
         $this->user = $user;
     }
-    
-    function auth($pass) {
+
+    public function auth($pass)
+    {
         if ($this->data['password'] === $pass) {
             return true;
         }
-        
+
         return false;
-        
     }
-    
-    function startSession() {
-        
+
+    public function startSession()
+    {
         $_SESSION['username'] = $this->user;
     }
-    
-    function doLogin() {
+
+    public function doLogin()
+    {
         session_regenerate_id();
         $_SESSION['loggedin'] = true;
         $_SESSION['ua'] = $_SERVER['HTTP_USER_AGENT'];
     }
-    
-    function doOTP() {
+
+    public function doOTP()
+    {
         $_SESSION['OTP'] = true;
     }
-    
-    function isOTP() {
+
+    public function isOTP()
+    {
         if (isset($_SESSION['OTP']) && $_SESSION['OTP'] == true) {
-            
             return true;
         }
+
         return false;
-        
     }
-    function isLoggedIn() {
+
+    public function isLoggedIn()
+    {
         if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true &&
             isset($_SESSION['ua']) && $_SESSION['ua'] == $_SERVER['HTTP_USER_AGENT']
         ) {
-            
+
             return $_SESSION['username'];
         }
         return false;
-        
+
     }
-    
-    
-    function getUsername() {
-        return $this->user;   
+
+    public function getUsername()
+    {
+        return $this->user;
     }
-    
-    function getSecret() {
+
+    public function getSecret()
+    {
         if (isset($this->data['secret'])) {
             return $this->data['secret'];
         }
+
         return false;
     }
-    
-    function generateSecret() {
+
+    public function generateSecret()
+    {
         $g = new GoogleAuthenticator();
         $secret = $g->generateSecret();
         $this->data['secret'] = $secret;
+
         return $secret;
-        
     }
-    
-    function getData() {
+
+    public function getData()
+    {
         return $this->data;
     }
-    
-    function setOTPCookie() {
-        $time = floor(time() / (3600 * 24) ); // get day number
+
+    public function setOTPCookie()
+    {
+        $time = floor(time() / (3600 * 24)); // get day number
         //about using the user agent: It's easy to fake it, but it increases the barrier for stealing and reusing cookies nevertheless
         // and it doesn't do any harm (except that it's invalid after a browser upgrade, but that may be even intented)
-        $cookie =  $time.":".hash_hmac("sha1",$this->getUsername().":".$time.":". $_SERVER['HTTP_USER_AGENT'],$this->getSecret());
-        setcookie ( "otp", $cookie, time() + (30 * 24 * 3600), null,null,null,true );
+        $cookie = $time . ":" . hash_hmac("sha1", $this->getUsername() . ":" . $time . ":" . $_SERVER['HTTP_USER_AGENT'], $this->getSecret());
+        setcookie("otp", $cookie, time() + (30 * 24 * 3600), null, null, null, true);
     }
-    
-    function hasValidOTPCookie() {
+
+    public function hasValidOTPCookie()
+    {
         // 0 = tomorrow it is invalid
         $daysUntilInvalid = 0;
-        $time = (string) floor((time() / (3600 * 24))) ; // get day number
+        $time = (string)floor((time() / (3600 * 24))); // get day number
         if (isset($_COOKIE['otp'])) {
-            list( $otpday,$hash) = explode(":",$_COOKIE['otp']);
-               
-            if ( $otpday >= $time - $daysUntilInvalid && $hash == hash_hmac('sha1',$this->getUsername().":".$otpday .":". $_SERVER['HTTP_USER_AGENT'] , $this->getSecret())
-                ) {
-                  return true;
+            list($otpday, $hash) = explode(":", $_COOKIE['otp']);
+
+            if ($otpday >= $time - $daysUntilInvalid && $hash == hash_hmac('sha1', $this->getUsername() . ":" . $otpday . ":" . $_SERVER['HTTP_USER_AGENT'], $this->getSecret())) {
+                return true;
             }
-                
-             
         }
+
         return false;
-    
     }
-    
 }
-?>
