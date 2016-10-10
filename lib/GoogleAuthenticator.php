508bb9ebebf2e91fbaa45cfcc32ef29b17987909
ReaGed <admin@freehacks.ru>
2016-10-10 18:16:33 +0300

    Update GoogleAuthenticator.php

--- lib/GoogleAuthenticator.php
+++ lib/GoogleAuthenticator.php
@@ -11,6 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+include_once("FixedByteNotation.php");
 
 class GoogleAuthenticator {
     static $PASS_CODE_LENGTH = 6;
